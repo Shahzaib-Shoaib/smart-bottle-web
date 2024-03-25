49dab754d2217bccdee02408ea9097c74bfb158b
Shahzaib Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2024-03-26 00:50:10 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,19 +6,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex flex-col  justify-center items-center">
-      <Link
-        href="/login"
-        className="text-white bg-indigo-500 border-0 py-2 px-8 mt-3 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-      >
-        Login
-      </Link>
-      <Link
-        href="/signup"
-        className="text-white bg-indigo-500 border-0 py-2 px-8 mt-3 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-      >
-        Signup
-      </Link>
-    </main>
+    <>
+      <div className="bg-gray-800 text-white py-4 ">
+        <h1 className="text-xl sm:text-xl md:text-xl lg:text-2xl ml-3 ">
+          Home{" "}
+        </h1>
+      </div>
+      <main className="flex flex-col  justify-center items-center mt-20">
+        <Link
+          href="/login"
+          className="text-white bg-indigo-500 border-0 py-2 px-8 mt-3 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        >
+          Login
+        </Link>
+        <Link
+          href="/signup"
+          className="text-white bg-indigo-500 border-0 py-2 px-8 mt-3 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        >
+          Signup
+        </Link>
+      </main>
+    </>
   );
 }
