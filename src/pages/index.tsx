bd33e94decb87f2032eb6c065e89f44165c4fdf8
Shahzaib Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2024-03-26 00:15:48 +0500

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,20 @@ import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
   return (
-    <main>
-      <Link href="/login">Login</Link>
-      <Link href="/signup">Signup</Link>
+    <main className="flex flex-col  justify-center items-center">
+      <Link
+        href="/login"
+        className="text-white bg-indigo-500 border-0 py-2 px-8 mt-3 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >
+        Login
+      </Link>
+      <Link
+        href="/signup"
+        className="text-white bg-indigo-500 border-0 py-2 px-8 mt-3 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >
+        Signup
+      </Link>
     </main>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -29,34 +29,47 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <form action="#" method="POST" onSubmit={login} className="form">
-      <div className="formGroup">
-        <label htmlFor="email">Email </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          ref={emailRef}
-          className="inputField"
-        />
-      </div>
+    <form
+      action="#"
+      method="POST"
+      onSubmit={login}
+      className="flex justify-center items-center align-middle"
+    >
+      <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 my-3 flex flex-col  w-full mt-10 md:mt-0">
+        <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+          Signup
+        </h2>
 
-      <div className="formGroup">
-        <label htmlFor="password">Password </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          ref={passwordRef}
-          className="inputField"
-        />
-      </div>
+        <div className="relative mb-4">
+          <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+            Email{" "}
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            ref={emailRef}
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
 
-      <div className="formGroup">
-        <button type="submit" className="submitButton">
-          Submit
-        </button>
-      </div>
+        <div className="relative mb-4">
+          <label htmlFor="password" className="leading-7 text-sm text-gray-600">
+            Password{" "}
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            ref={passwordRef}
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+
+          <button type="submit" className="text-white bg-indigo-500 border-0 py-2 px-8 mt-3 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Submit
+          </button>
+        </div>
     </form>
   );
 };
